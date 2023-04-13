b1c67b4ccb244bcf9a0b2b392d975916c4022ff8
Vic <piton20091986@gmail.com>
2023-04-13 22:52:56 +0300

    first commit

---
+++ src/Pages/AuthBoard.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function AuthBoard() {
+    return (
+        <div className="Alert-box">
+            <h1>Вход в Чат</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda asperiores non eos quaerat unde est laborum dolore ad eaque officia! Totam quia iure aut tempore obcaecati reprehenderit, consequatur fugit ut.</p>
+        </div>
+    )
+}
+
+export default AuthBoard
